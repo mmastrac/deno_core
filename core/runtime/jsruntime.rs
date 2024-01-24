74ece09737d1c671fef65245228350a4ae3f5246
Matt Mastracci <matthew@mastracci.com>
2024-01-24 15:08:04 -0500

    chore: copyright bump

--- core/runtime/jsruntime.rs
+++ core/runtime/jsruntime.rs
@@ -1,4 +1,4 @@
-// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 use super::bindings;
 use super::bindings::v8_static_strings;
 use super::bindings::watch_promise;
--- testing/unit/stats_test.ts
+++ testing/unit/stats_test.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 import { barrierAwait, barrierCreate, StatsFactory } from "checkin:async";
 import { assert, assertEquals, test } from "checkin:testing";
 
